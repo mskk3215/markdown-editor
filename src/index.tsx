043371cf1535d5dc435a42c973898494e8377e00
mskk3215 <m.kaaiweb@gmail.com>
2022-11-06 00:42:33 +0900

    useState add

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
 import { render } from "react-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import { Editor } from "./pages/editor";
 
 // Main という変数に React のビューを書いています。これが JSX
-const Main = <h1>Markdown Editor</h1>;
+
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
+
+`;
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+);
 
 // app という ID を持つ HTML 内の要素に対して Main という変数の内容を紐付ける処理
 render(Main, document.getElementById("app"));
