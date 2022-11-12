41d57c5a18d57bfc16f515a5378ea8d2a6a1af9f
mskk3215 <m.kaaiweb@gmail.com>
2022-11-12 23:01:44 +0900

    Route add

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
+// react-router には HashRouter と BrowserRouter の２つが用意されています。
+// HashRouter は、上記のように # の後にパスを記述する方式です。もともと URL に含まれる # は「フラグメント」と呼ばれ、ページ内の特定の箇所を指しています。
+import { Redirect, Route, HashRouter as Router } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
 import { Editor } from "./pages/editor";
 
@@ -13,8 +16,22 @@ const GlobalStyle = createGlobalStyle`
 `;
 const Main = (
   <>
+    {/* 例えばフッターなどをこのRouteタグの外側に配置すると、パスに関わらず固定表示するといったことができます。 */}
+    {/* exact 属性は、パスの判定方法を切り替えるパラメーターです。
+    exact={true} の場合は完全一致、exact={false} は部分一致になります。
+    例えば /editor で exact={false} の場合、/editor/foo でも一致と判定されます。
+    指定がない場合は exact={false} がデフォルトの挙動となります。 */}
     <GlobalStyle />
-    <Editor />
+    <Router>
+      <Route exact path="/editor">
+        <Editor />
+      </Route>
+      <Route exact path="/history">
+        <h1>History</h1>
+      </Route>
+      {/* 定義されていないパスの場合は /editor にリダイレクトする、という定義 */}
+      <Redirect to="/editor" path="*" />
+    </Router>
   </>
 );
 
